0d08765da1edfa0b5d4ca5ea168f79d38e054c6d
travisdotdev <travis4yusuf@gmail.com>
2025-12-04 12:40:46 +0000

    file content retrieval working

--- get_file_content.py
+++ get_file_content.py
@@ -7,14 +7,26 @@ def get_file_content(working_directory, file_path):
     try:
         abs_working = os.path.abspath(working_directory)
         abs_target = os.path.abspath(os.path.join(working_directory, file_path))
-        full_path = os.path.join(abs_target, file_path)
+        full_path = os.path.abspath(os.path.join(working_directory, file_path))
+        file_path = os.path.abspath(full_path)
+        # print("HERE======================")
+        # print(f"Abs_working {abs_working}")
+        # print(f"Abs_target: {abs_target}")
+        # print(f"Full Path: {full_path}")
+        # print(f"File_path: {file_path}")
+        # print("END==========================")
         if not abs_target.startswith(abs_working):
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        elif not os.path.isdir(full_path):
-            return f'Error file not found or is not a regular file: "{full_path}"'
+        elif os.path.isdir(file_path):
+            return f'Error file not found or is not a regular file: "{file_path}"'
         else:
-            with open(full_path, "r") as f:
+            # print("WE REACHED FILE PRINTING")
+            with open(file_path, "r") as f:
                 file_content_string = f.read(MAX_CHARS)
+                if len(file_content_string) == MAX_CHARS:
+                    return f'{file_content_string}\n[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                else:
+                    return file_content_string
 
     except Exception as e:
         return f"Error: {str(e)}"
